3468ef59086dc85172d7fc5bd4247256fa8b2324
Sean Mena <seanmena13@gmail.com>
2024-03-19 18:34:56 -0600

    updated about-me.js

--- src/screens/AboutMe.js
+++ src/screens/AboutMe.js
@@ -16,22 +16,22 @@ export default function AboutMe() {
         for creating original, responsive, and impressive full-stack
         applications. To bring value to my professional endeavors, I spend the
         majority of my free time educating myself on development technologies
-        and best practices. I regularly set aside time to keep up with the ever
-        changing field of development to ensure I am always up to date with what
-        is current to be as secure and efficient as possible with my code. I
-        have been creating webpages and online stores with tools like Shopify
-        and SqaureSpace for about 2 years until graduating from the University
-        of Utah with a Professional Education Award in Full-Stack Web
-        Development in January of 2021. I have been coding independently with
-        freelance projects since then, and have learned a lot about how much
-        goes in to each part of a website. I plan to continue traditional
-        university studies to obtain a Bachelor's Degree in Graphic Design as
-        well. If I'm not coding or working on projects, I spend my free time
-        using the Adobe Suite to create designs and different pieces of artwork.
-        I also love making music, coffee, traveling, and spending time outdoors!
-        I enjoy combining my passions by creating artwork for my applications. I
-        am confident in my ability to work in a team, meet goals, and promptly
-        learn any language. I am also Fluent in both Spanish and English.
+        and best practices. I regularly set aside time to keep up with the
+        current techonologies to ensure I am always up to date and as efficient
+        as possible with my code. I have been creating webpages and online
+        stores with tools like Shopify and SqaureSpace for about 2 years until
+        graduating from the University of Utah with a Professional Education
+        Award in Full-Stack Web Development in January of 2021. I have been
+        coding independently with freelance projects, and have learned a lot
+        about how much goes in to each part of a website. I plan to continue
+        traditional university studies to obtain a Bachelor's Degree in Computer
+        Science as well. If I'm not coding or working on projects, I spend my
+        free time using the Adobe Suite to create designs and different pieces
+        of artwork. I also love making music, coffee, traveling, and spending
+        time outdoors! I enjoy combining my passions by creating artwork for my
+        applications. I am confident in my ability to work in a team, meet
+        goals, and promptly learn any language. I am also Fluent in both Spanish
+        and English.
       </p>
 
       <div className="foot">
